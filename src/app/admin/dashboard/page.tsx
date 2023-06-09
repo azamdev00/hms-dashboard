da3f7ff09591d611b58d79554e559f90e161c80d
Azam <azamdev00@gmail.com>
2023-06-09 11:28:31 +0500

    add page for dashboard

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import type { FC } from "react";
 
-const Prescriptions: FC = () => {
+const Dashboard: FC = () => {
   return (
     <>
       <div className="block items-center justify-between border-b border-gray-200 bg-white p-4">
         <div className="mb-1 w-full">
           <div className="mb-4">
             <h1 className="text-xl font-semibold text-gray-900  sm:text-2xl">
-              Prescriptions
+              Dashboard
             </h1>
           </div>
         </div>
@@ -17,4 +17,4 @@ const Prescriptions: FC = () => {
   );
 };
 
-export default Prescriptions;
+export default Dashboard;
