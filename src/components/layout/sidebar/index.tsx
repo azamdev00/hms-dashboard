e253f4a7e6b727c9dbe306dc3ed40e802cc9bf98
Azam <azamdev00@gmail.com>
2023-06-28 14:56:55 +0500

    remove items from side bar

--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -1,15 +1,7 @@
-import { Sidebar, TextInput } from "flowbite-react";
+import { Sidebar } from "flowbite-react";
 import type { FC } from "react";
 import { useEffect, useState } from "react";
-import {
-  HiChartPie,
-  HiClipboard,
-  HiCollection,
-  HiInformationCircle,
-  HiSearch,
-  HiShoppingBag,
-  HiUsers,
-} from "react-icons/hi";
+import { HiChartPie, HiShoppingBag, HiUsers } from "react-icons/hi";
 
 const LayoutSidebar: FC = function () {
   const [currentPage, setCurrentPage] = useState("");
@@ -23,73 +15,47 @@ const LayoutSidebar: FC = function () {
   return (
     <Sidebar aria-label="Sidebar with multi-level dropdown example">
       <div className="flex h-full flex-col justify-between py-2">
-        <div>
-          <form className="pb-3 md:hidden">
-            <TextInput
-              icon={HiSearch}
-              type="search"
-              placeholder="Search"
-              required
-              size={32}
-            />
-          </form>
-          <Sidebar.Items>
-            <Sidebar.ItemGroup>
-              <Sidebar.Item
-                href="/"
-                icon={HiChartPie}
-                className={"/" === currentPage ? "bg-gray-100" : ""}
-              >
-                Dashboard
-              </Sidebar.Item>
-              <Sidebar.Item
-                href="/admin/prescriptions"
-                icon={HiShoppingBag}
-                className={
-                  "/admin/prescriptions" === currentPage ? "bg-gray-100" : ""
-                }
-              >
-                Prescriptions
-              </Sidebar.Item>
-              <Sidebar.Item
-                href="/admin/doctors"
-                icon={HiUsers}
-                className={
-                  "/admin/doctors" === currentPage ? "bg-gray-100" : ""
-                }
-              >
-                Doctors
-              </Sidebar.Item>
-              <Sidebar.Item
-                href="/admin/patients"
-                icon={HiUsers}
-                className={
-                  "/admin/patients" === currentPage ? "bg-gray-100" : ""
-                }
-              >
-                Patients
-              </Sidebar.Item>
-              <Sidebar.Item
-                href="/admin/opds"
-                icon={HiUsers}
-                className={"/admin/opds" === currentPage ? "bg-gray-100" : ""}
-              >
-                OPD
-              </Sidebar.Item>
-            </Sidebar.ItemGroup>
-            <Sidebar.ItemGroup>
-              <Sidebar.Item href="/" icon={HiClipboard}>
-                Docs
-              </Sidebar.Item>
-              <Sidebar.Item href="/" icon={HiCollection}>
-                Components
-              </Sidebar.Item>
-              <Sidebar.Item href="/" icon={HiInformationCircle}>
-                Help
-              </Sidebar.Item>
-            </Sidebar.ItemGroup>
-          </Sidebar.Items>
-        </div>
+        <Sidebar.Items>
+          <Sidebar.ItemGroup>
+            <Sidebar.Item
+              href="/"
+              icon={HiChartPie}
+              className={"/" === currentPage ? "bg-gray-100" : ""}
+            >
+              Dashboard
+            </Sidebar.Item>
+            <Sidebar.Item
+              href="/admin/prescriptions"
+              icon={HiShoppingBag}
+              className={
+                "/admin/prescriptions" === currentPage ? "bg-gray-100" : ""
+              }
+            >
+              Prescriptions
+            </Sidebar.Item>
+            <Sidebar.Item
+              href="/admin/doctors"
+              icon={HiUsers}
+              className={"/admin/doctors" === currentPage ? "bg-gray-100" : ""}
+            >
+              Doctors
+            </Sidebar.Item>
+            <Sidebar.Item
+              href="/admin/patients"
+              icon={HiUsers}
+              className={"/admin/patients" === currentPage ? "bg-gray-100" : ""}
+            >
+              Patients
+            </Sidebar.Item>
+            <Sidebar.Item
+              href="/admin/opds"
+              icon={HiUsers}
+              className={"/admin/opds" === currentPage ? "bg-gray-100" : ""}
+            >
+              OPD
+            </Sidebar.Item>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
       </div>
     </Sidebar>
   );
