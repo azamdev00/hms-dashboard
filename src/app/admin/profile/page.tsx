a16b74aeb72162672c6cd0f020f2042247312935
Azam <azamdev00@gmail.com>
2023-07-02 20:56:55 +0500

    add input for profiel

--- src/app/admin/profile/page.tsx
+++ src/app/admin/profile/page.tsx
@@ -106,12 +106,10 @@ const Profile: NextPage = () => {
                           clip-rule="evenodd"
                         />
                       </svg>
-                      <button
-                        type="button"
+                      <input
+                        type="file"
                         className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                      >
-                        Change
-                      </button>
+                      />
                     </div>
                   </div>
                 </div>
